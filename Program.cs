3cb1068eee9e8bf62e18edc23c276c7d315d6b25
etec adolpho berezin <e107ti@cps.sp.gov.br>
2022-11-10 22:01:37 -0300

    Resolução do Exerc´cio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Números pares entre 0 e ? ");
+int numeroInformado = Convert.ToInt32(Console.ReadLine());
+
+if (numeroInformado >= 1)
+{
+    int numeroPar = 0;
+
+    while (numeroPar <= numeroInformado)
+    {
+        Console.Write($"{numeroPar} ");
+        numeroPar += 2;
+    }
+}
+else
+{
+    Console.WriteLine($"O número {numeroInformado} não é inteiro positivo.");
+}
